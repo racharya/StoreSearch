02c71039f9ba6a1900d864f2cc6e2f2bec045de0
Rachana Acharya <rasnacharya@gmail.com>
2015-08-15 15:26:30 -0600

    Added logic to display fake search results; dismiss keyboard once search is clicked

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -9,17 +9,58 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    var searchResults = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //tells table view to add a 64 pt margin at the top, (20 for status bar and 44 for search bar)
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+}//end of SearchViewController
 
-}
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        
+        searchBar.resignFirstResponder() // tells UISearchBar that it should no longer listen to keyboard input
+        println("The search text is: '\(searchBar.text)'")
+        
+        searchResults = [String]()
+        for i in 0...2 {
+            searchResults.append(String(format: "Fake Result %d for '%@'", i, searchBar.text))
+        }
+        tableView.reloadData()
+    }
+}//end of extension UISearchBarDelegate
 
+extension SearchViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "SearchResultCell"
+        var cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as! UITableViewCell!
+        
+        if cell == nil {
+            cell = UITableViewCell(style: .Default, reuseIdentifier: cellIdentifier)
+        }
+        cell.textLabel!.text = searchResults[indexPath.row]
+        return cell
+    }
+}//end of UITableViewDataSource
+
+extension SearchViewController: UITableViewDelegate {
+    
+}//end of UITableViewDelegate
