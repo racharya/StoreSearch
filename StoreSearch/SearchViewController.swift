abb88d55049989138d74af7a69d8fd908303728d
Rachana Acharya <rasnacharya@gmail.com>
2015-08-16 01:52:17 -0600

    Added logic to use custom cell

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -14,19 +14,23 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     var searchResults = [SearchResult]()
     var hasSearched = false
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //tells table view to add a 64 pt margin at the top, (20 for status bar and 44 for search bar)
         tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
+        //load nib, ask table view to register it using identifier
+        var cellNib = UINib(nibName: "SearchResultCell", bundle: nil)
+        tableView.registerNib(cellNib, forCellReuseIdentifier: "SearchResultCell")
+        tableView.rowHeight = 80
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
 }//end of SearchViewController
 
 extension SearchViewController: UISearchBarDelegate {
@@ -39,12 +43,12 @@ extension SearchViewController: UISearchBarDelegate {
         //testing if app can handle no results found situation
         if searchBar.text != "justin bieber" {
             
-        for i in 0...2 {
-            let searchResult = SearchResult()
-            searchResult.name = String(format:"Fake Result %d for",i)
-            searchResult.artistName = searchBar.text
-            searchResults.append(searchResult)
-        }
+            for i in 0...2 {
+                let searchResult = SearchResult()
+                searchResult.name = String(format:"Fake Result %d for",i)
+                searchResult.artistName = searchBar.text
+                searchResults.append(searchResult)
+            }
         }
         hasSearched = true
         tableView.reloadData()
@@ -64,25 +68,21 @@ extension SearchViewController: UITableViewDataSource {
         } else if searchResults.count == 0 {
             return 1
         } else {
-        return searchResults.count
+            return searchResults.count
         }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        let cellIdentifier = "SearchResultCell"
-        var cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as! UITableViewCell!
+        let cell = tableView.dequeueReusableCellWithIdentifier("SearchResultCell", forIndexPath: indexPath) as! SearchResultCell
         
-        if cell == nil {
-            cell = UITableViewCell(style: .Subtitle, reuseIdentifier: cellIdentifier)
-        }
         if searchResults.count == 0 {
-            cell.textLabel!.text = "(Nothing found)"
-            cell.detailTextLabel!.text = ""
+            cell.nameLabel.text = "(Nothing found)"
+            cell.artistNameLabel.text = ""
         } else {
-        let searchResult = searchResults[indexPath.row]
-        cell.textLabel!.text = searchResult.name
-        cell.detailTextLabel!.text = searchResult.artistName
+            let searchResult = searchResults[indexPath.row]
+            cell.nameLabel.text = searchResult.name
+            cell.artistNameLabel.text = searchResult.artistName
         }
         return cell
     }
