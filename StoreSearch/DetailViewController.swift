63032f86e1da3c8ca112d80c0300f6017c9391a5
Rachana Acharya <rasnacharya@gmail.com>
2015-08-23 13:00:32 -0600

    Added a new file to help pop-up detail when tap on row

---
+++ StoreSearch/DetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DetailViewController.swift
+//  StoreSearch
+//
+//  Created by Rachana Acharya on 8/23/15.
+//  Copyright (c) 2015 Rachana Acharya. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func close() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+  
+
+}
