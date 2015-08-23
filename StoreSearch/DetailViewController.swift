2725d79b2a7d5607a4d1d12ba96c481b519c7c25
Rachana Acharya <rasnacharya@gmail.com>
2015-08-23 14:25:57 -0600

    Extended delegate to perform transition to Detail view controller

--- StoreSearch/DetailViewController.swift
+++ StoreSearch/DetailViewController.swift
@@ -22,6 +22,23 @@ class DetailViewController: UIViewController {
     @IBAction func close() {
         dismissViewControllerAnimated(true, completion: nil)
     }
-  
+    
+    //loads the view controller from the storyboard
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        //tells UIKit that this view controller uses custom presentation
+        modalPresentationStyle = .Custom
+        transitioningDelegate = self
+    }
+}//end of DetailViewController: UIViewController
 
-}
+
+extension DetailViewController: UIViewControllerTransitioningDelegate {
+    
+    //tells UIKit what objectes to use to perform the transition to the Detail View Controller
+    //uses DimingPresentationController instead of standard presentation controller
+    func presentationControllerForPresentedViewController(presented: UIViewController, presentingViewController presenting: UIViewController!, sourceViewController source: UIViewController) -> UIPresentationController? {
+        return DimmingPresentationController(presentedViewController: presented, presentingViewController: presenting)
+    }
+    
+}//end of DetailViewController: UIViewControllerTransitioningDelegate
