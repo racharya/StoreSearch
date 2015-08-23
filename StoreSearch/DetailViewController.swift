7a45893ea0d03544e0253ef5ad2feb7c3a9cfff8
Rachana Acharya <rasnacharya@gmail.com>
2015-08-23 16:24:50 -0600

    Added line to set new tintColor on view and a line to set the coner radius

--- StoreSearch/DetailViewController.swift
+++ StoreSearch/DetailViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var popupView: UIView!
+    @IBOutlet weak var artworkImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var kindLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var priceButton: UIButton!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-
+         popupView.layer.cornerRadius = 10//gives round corners to the popupview
+        view.tintColor = UIColor(red:20/255, green: 160/255, blue: 160/255, alpha:1)
+       
     }
 
     override func didReceiveMemoryWarning() {
