ef2cb02f3badc4ac1b105aa978d08109a6af7b09
Rachana Acharya <rasnacharya@gmail.com>
2015-08-22 23:46:05 -0600

    Added logic to load cell's image view with an image from artworkURL60

--- StoreSearch/SearchResultCell.swift
+++ StoreSearch/SearchResultCell.swift
@@ -12,6 +12,7 @@ class SearchResultCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var artistNameLabel: UILabel!
     @IBOutlet weak var artworkImageView: UIImageView!
+    var downloadTask: NSURLSessionDownloadTask?
 
     //called after the cell obj has been loaded from the nib but before the cell
     //is added to the table view
@@ -28,5 +29,46 @@ class SearchResultCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    func configureForSearchResult(searchResult: SearchResult) {
+        nameLabel.text = searchResult.name
+        if searchResult.artistName.isEmpty {
+            artistNameLabel.text = "Unknown"
+        } else {
+            artistNameLabel.text = String(format: "%@ (%@)", searchResult.artistName, kindForDisplay(searchResult.kind))
+        }
+        //tells UIImageView to load the image from artworkURL60 and place it in the cell
+        artworkImageView.image = UIImage(named: "Placeholder")
+        if let url = NSURL(string: searchResult.artworkURL60) {
+            downloadTask = artworkImageView.loadImageWithURL(url)
+        }
+    }
+    
+    //converts internal identifier to the text we want to show to the user
+    func kindForDisplay(kind: String) -> String {
+        switch kind {
+        case "album": return "Album"
+        case "audiobook": return "Audio Book"
+        case "book": return "Book"
+        case "ebook": return "E-Book"
+        case "feature-movie": return "Movie"
+        case "music-video": return "Music Video"
+        case "podcast": return "Podcast"
+        case "software": return "App"
+        case "song": return "Song"
+        case "tv-episode": return "TV Episode"
+        default: return kind
+        }
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        println("In the prepareForReuseMethod")
+        downloadTask?.cancel()
+        downloadTask = nil
+        nameLabel.text = nil
+        artistNameLabel.text = nil
+        artworkImageView.image = nil
+    }
 
-}
+}// end of SearchResultCell class
