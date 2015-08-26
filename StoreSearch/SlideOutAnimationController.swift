81d96b506c243fc818e00cd20181fa7d1499a084
Rachana Acharya <rasnacharya@gmail.com>
2015-08-26 00:00:19 -0600

    Added new files

---
+++ StoreSearch/SlideOutAnimationController.swift
@@ -0,0 +1,9 @@
+//
+//  SlideOutAnimationController.swift
+//  StoreSearch
+//
+//  Created by Rachana Acharya on 8/25/15.
+//  Copyright (c) 2015 Rachana Acharya. All rights reserved.
+//
+
+import Foundation
