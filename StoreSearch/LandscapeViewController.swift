93d080ed8684badb6e03f42360521408d93e5ec3
Rachana Acharya <rasnacharya@gmail.com>
2015-08-26 23:35:30 -0600

    Put image on the scroll view's background and set its contentSize

--- StoreSearch/LandscapeViewController.swift
+++ StoreSearch/LandscapeViewController.swift
@@ -10,8 +10,22 @@ import UIKit
 
 class LandscapeViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.removeConstraints(view.constraints())//getting rid of storyboard's automatic constraints
+        view.setTranslatesAutoresizingMaskIntoConstraints(true)//positioning and sizing our views manually
+        
+        pageControl.removeConstraints(pageControl.constraints())
+        pageControl.setTranslatesAutoresizingMaskIntoConstraints(true)
+        
+        scrollView.removeConstraints(scrollView.constraints())
+        scrollView.setTranslatesAutoresizingMaskIntoConstraints(true)
+        scrollView.backgroundColor = UIColor(patternImage: UIImage(named: "LandscapeBackground")!)
+        scrollView.contentSize = CGSize(width: 1000, height: 1000)
 
         // Do any additional setup after loading the view.
     }
@@ -26,6 +40,17 @@ class LandscapeViewController: UIViewController {
         println("deinit \(self)")
     }
     
+    //Creating our own layout
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        scrollView.frame = view.bounds
+        
+        pageControl.frame = CGRect(x: 0,
+            y: view.frame.size.height - pageControl.frame.size.height,
+            width:view.frame.size.height,
+            height: pageControl.frame.size.height)
+        
+    }
 
 
-}
+}// end of LandscapeViewController
