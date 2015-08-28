12e9519486661705ec5826c0487788710ef10ea7
Rachana Acharya <rasnacharya@gmail.com>
2015-08-28 01:16:12 -0600

    Paste methods that were removed from SearchViewController

--- StoreSearch/Search.swift
+++ StoreSearch/Search.swift
@@ -16,6 +16,197 @@ class Search {
     private var dataTask: NSURLSessionDataTask? = nil
     
     func performSearchForText(text: String, category: Int) {
-        println("Searching...")
+        if !text.isEmpty{
+            dataTask?.cancel()
+            
+            isLoading = true
+            hasSearched = true
+            searchResults = [SearchResult]()
+            
+            let url = urlWithSearchText(text, category: category)
+            
+            let session = NSURLSession.sharedSession()
+            dataTask = session.dataTaskWithURL(url, completionHandler: {data, response, error in
+                if let error = error {
+                    if error.code == -999 {return} //Search was cancelled
+                } else if let httpResponse = response as? NSHTTPURLResponse {
+                    if httpResponse.statusCode == 200 {
+                        if let dictionary = self.parseJSON(data) {
+                            self.searchResults = self.parseDictionary(dictionary)
+                            self.searchResults.sort(<)
+                            
+                            println("Success!")
+                            self.isLoading = false
+                            return
+                        }
+                    }
+                }
+                println("Failure! \(response)")
+                self.hasSearched = false
+                self.isLoading = false
+            })
+            dataTask?.resume()
+        }
     }
-}
+    
+    
+    //Builds url as a string by placing the text from the search bar behind the "term=" param and then returns this string into an NSURL obj
+    private func urlWithSearchText(searchText: String, category: Int) -> NSURL {
+        
+        var entityName: String
+        //turns category index from a number into a string
+        switch category {
+        case 1: entityName = "musicTrack"
+        case 2: entityName = "software"
+        case 3: entityName = "ebook"
+        default: entityName = ""
+        }
+        
+        let escapedSearchText = searchText.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!//escape the special characters
+        let urlString = String(format: "http://itunes.apple.com/search?term=%@&limit=200&entity=%@", escapedSearchText, entityName)
+        let url = NSURL(string: urlString)
+        //force unwrapping failable initializers to return an actual NSURL obj
+        return url!
+    }
+    
+    private func parseJSON(data: NSData) -> [String: AnyObject]? { // returns dictionary of type [String: AnyObject]
+        
+        var error: NSError?
+        if let json = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(0), error: &error) as? [String: AnyObject] {
+            return json
+        } else if let error = error {
+            println("JSON Error: \(error)")
+        } else {
+            println("Unknown JSON Error")
+        }
+        return nil
+    }
+    
+    // goes through top-level dictionary and looks at each search result in turn
+    private func parseDictionary(dictionary: [String: AnyObject]) ->  [SearchResult] {
+        
+        var searchResults = [SearchResult]()
+        //1.making sure dictionary has a key named "results" that contains an array
+        if let array: AnyObject = dictionary["results"] {
+            //2. Loop through the each items in the array(each items are dictionary)
+            for resultDict in array as! [AnyObject] {
+                //3.casting each dictionary items(type AnyObject) to the right type(AnyObject)
+                if let resultDict = resultDict as? [String: AnyObject] {
+                    var searchResult: SearchResult?
+                    //if found item is a "track" then create a SearchResult object using parseTrack()
+                    if let wrapperType = resultDict["wrapperType"] as? String {
+                        switch wrapperType {
+                        case "track":
+                            searchResult = parseTrack(resultDict)
+                        case "audiobook":
+                            searchResult = parseAudioBook(resultDict)
+                        case "software":
+                            searchResult = parseSoftware(resultDict)
+                        default:
+                            break // does nothing and moves along
+                        }
+                    }
+                        //since ebook doesnot have a wrapperType field
+                    else if let kind = resultDict["kind"] as? String {
+                        if kind == "ebook" {
+                            searchResult = parseEBook(resultDict)
+                        }
+                    }
+                    if let result = searchResult {
+                        searchResults.append(result) //add to searchResults array
+                    }
+                    //5. If something went wrong, print out a message for debugging purposes
+                } else {
+                    println("Expected a dictionary")
+                }
+            }
+        } else {
+            println("Expected 'results' array")
+        }
+        return searchResults
+    }
+    
+    
+    //instantiate a new SearchResult object then get the values out of the dictionary and put them in the SearchResult's properties
+    private func parseTrack(dictionary: [String : AnyObject]) -> SearchResult {
+        
+        let searchResult = SearchResult()
+        
+        searchResult.name = dictionary["trackName"] as! String
+        searchResult.artistName = dictionary["artistName"] as! String
+        searchResult.artworkURL60 = dictionary["artworkUrl60"] as! String
+        searchResult.artworkURL100 = dictionary["artworkUrl100"] as! String
+        searchResult.storeURL = dictionary["trackViewUrl"] as! String
+        searchResult.kind = dictionary["kind"] as! String
+        searchResult.currency = dictionary["currency"] as! String //cast price (number) to string
+        
+        if let price = dictionary["trackPrice"] as? Double { // then cast price to double
+            searchResult.price = price
+        }
+        if let genre = dictionary["primaryGenreName"] as? String {
+            searchResult.genre = genre
+        }
+        return searchResult
+    }
+    
+    private func parseAudioBook(dictionary: [String : AnyObject]) -> SearchResult {
+        
+        let searchResult = SearchResult()
+        searchResult.name = dictionary["collectionName"] as! String
+        searchResult.artistName = dictionary["artistName"] as! String
+        searchResult.artworkURL60 = dictionary["artworkUrl60"] as! String
+        searchResult.artworkURL100 = dictionary["artworkUrl100"] as! String
+        searchResult.storeURL = dictionary["collectionViewUrl"] as! String
+        searchResult.kind = "audiobook" //AudioBooks don't have a kind so manually set it
+        searchResult.currency = dictionary["currency"] as! String
+        
+        if let price = dictionary["collectionPrice"] as? Double {
+            searchResult.price = price
+        }
+        if let genre = dictionary["primaryGenreName"] as? String {
+            searchResult.genre = genre
+        }
+        return searchResult
+    }
+    
+    func parseSoftware(dictionary: [String: AnyObject]) -> SearchResult {
+        let searchResult = SearchResult()
+        searchResult.name = dictionary["trackName"] as! String
+        searchResult.artistName = dictionary["artistName"] as! String
+        searchResult.artworkURL60 = dictionary["artworkUrl60"] as! String
+        searchResult.artworkURL100 = dictionary["artworkUrl100"] as! String
+        searchResult.storeURL = dictionary["trackViewUrl"] as! String
+        searchResult.kind = dictionary["kind"] as! String
+        searchResult.currency = dictionary["currency"] as! String
+        
+        if let price = dictionary["price"] as? Double {
+            searchResult.price = price
+        }
+        if let genre = dictionary["primaryGenreName"] as? String {
+            searchResult.genre = genre
+        }
+        return searchResult
+    }
+    
+    private func parseEBook(dictionary: [String: AnyObject]) -> SearchResult {
+        
+        let searchResult = SearchResult()
+        searchResult.name = dictionary["trackName"] as! String
+        searchResult.artistName = dictionary["artistName"] as! String
+        searchResult.artworkURL60 = dictionary["artworkUrl60"] as! String
+        searchResult.artworkURL100 = dictionary["artworkUrl100"] as! String
+        searchResult.storeURL = dictionary["trackViewUrl"] as! String
+        searchResult.kind = dictionary["kind"] as! String
+        searchResult.currency = dictionary["currency"] as! String
+        if let price = dictionary["price"] as? Double {
+            searchResult.price = price
+        }
+        if let genres: AnyObject = dictionary["genres"] {
+            //ebook dont have a genre so manually set it using join()
+            searchResult.genre = ", ".join(genres as! [String])
+        }
+        return searchResult
+    }
+    
+    
+}// end of Search class
