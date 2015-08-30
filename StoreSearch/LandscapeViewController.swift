d0f3c31db029acdaffac4bfbdee528c32198502c
Rachana Acharya <rasnacharya@gmail.com>
2015-08-29 23:50:29 -0600

    Added method to hide the spinner after search is complete

--- StoreSearch/LandscapeViewController.swift
+++ StoreSearch/LandscapeViewController.swift
@@ -63,7 +63,7 @@ class LandscapeViewController: UIViewController {
             case .NotSearchedYet:
                 break
             case .Loading:
-                break
+                showSpinner()
             case .NoResults:
                 break
             case .Results(let list):
@@ -175,6 +175,29 @@ class LandscapeViewController: UIViewController {
             downloadTask.resume()
             downloadTasks.append(downloadTask)
         }
+    }
+    
+    //shows spinner when search is taking place when device is flipped to Landscape view
+    private func showSpinner() {
+        let spinner = UIActivityIndicatorView( activityIndicatorStyle: .WhiteLarge)
+        spinner.center = CGPoint(x: CGRectGetMidX(scrollView.bounds) + 0.5, y:CGRectGetMidY(scrollView.bounds) + 0.5)
+        spinner.tag = 1000
+        view.addSubview(spinner)
+        spinner.startAnimating()
+    }
+    
+    func searchResultsReceived(){
+        hideSpinner()
+        switch search.state {
+        case .NotSearchedYet, .Loading, .NoResults:
+            break
+        case .Results(let list):
+            tileButtons(list)
+        }
+    }
+    
+    private func hideSpinner() {
+        view.viewWithTag(1000)?.removeFromSuperview()
     }
   
 
